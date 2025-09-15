2e4c8c6e61e2d5f888abe716f4a916490dd62c43
R-onit <ronitpatil0103@gmail.com>
2025-09-15 14:28:55 +0530

    fix(core):removed the unnecessary declared registers

---
+++ 01_memory_mapped_struct_led/Src/main.c
@@ -0,0 +1,52 @@
+
+#define __IO  			volatile
+
+#include <stdint.h>
+
+#define PERIPH_BASE         (0x40000000UL)
+
+/* --- Bus base addresses ---*/
+#define APB2PERIPH_BASE     (PERIPH_BASE + 0x10000UL)   // 0x40010000
+#define AHBPERIPH_BASE      (PERIPH_BASE + 0x20000UL)   // 0x40020000
+
+/* --- Peripheral base addresses ---*/
+#define GPIOA_BASE          (APB2PERIPH_BASE + 0x0800UL) // 0x40010800
+#define RCC_BASE            (AHBPERIPH_BASE  + 0x1000UL) // 0x40021000
+
+/* --- Bit positions ---*/
+#define GPIOAEN             (1U << 2)
+#define LED_PIN             (1U << 5)
+
+typedef struct
+{
+  volatile uint32_t DUMMY[5];
+  volatile uint32_t AHBENR;
+  volatile uint32_t APB2ENR;
+} RCC_TypeDef;
+
+typedef struct
+{
+  volatile uint32_t CRL;
+  volatile uint32_t DUMMY[2];
+  volatile uint32_t ODR;
+
+} GPIO_TypeDef;
+
+#define RCC 		((RCC_TypeDef *)RCC_BASE)
+#define GPIOA		((GPIO_TypeDef *)GPIOA_BASE)
+
+int main(void)
+{
+    // 1. Enable GPIOA clock
+	RCC->APB2ENR |=GPIOAEN;
+    // 2. Configure PA5 as output (2MHz push-pull)
+	GPIOA->CRL &= ~(0xF << (4*5));		// clear bits for PA5
+	GPIOA->CRL |= (0x2 << (4*5)); 		//  CNF=00 ,MODE=10,
+    // 3. Toggle LED
+    while (1)
+    {
+    	GPIOA->ODR ^= LED_PIN;
+    	for (volatile int i = 0; i < 100000; i++);
+    }
+}
+
