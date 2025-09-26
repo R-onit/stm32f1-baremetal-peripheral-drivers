16d6689ab5aaf501087991c3798876fe7cafb346
R-onit <ronitpatil0103@gmail.com>
2025-09-26 07:02:07 +0530

    feat: configured and added adc single conversion drivers

--- 05_adc/Inc/adc.h
+++ 05_adc/Inc/adc.h
@@ -1,6 +1,7 @@
 #ifndef ADC_H_
 #define ADC_H_
-
-
-
+#include <stdint.h>
+void pa1_adc_init(void);
+void start_conversion(void);
+uint32_t read_conversion(void);
 #endif
--- 05_adc/Src/main.c
+++ 05_adc/Src/main.c
@@ -1,18 +1,25 @@
+#include <stdio.h>
 #include "stm32f103xb.h"
+#include "adc.h"
 #include "uart.h"
 
+/* global so debugger can always inspect it; volatile prevents optimization */
+volatile uint32_t sensor_value;
 
-int main(void){
-	uar2_rxtx_init();
+int main(void)
+{
+    /* init peripherals ONCE */
+    uar2_rxtx_init();
+    pa1_adc_init();
 
-	while(1){
-		char recieved = USART2_read();
-		USART2_write(recieved);
+    /* continuous read + print */
+    while (1)
+    {
+        start_conversion();
+        sensor_value = read_conversion();               // debugger-friendly global volatile
+		printf("sensor_value: %d\r\n", (int)sensor_value);
 
-	}
+        /* simple delay so terminal is readable */
+        for (volatile int i = 0; i < 300000; ++i) { __asm("nop"); }
+    }
 }
-
-
-
-
-
