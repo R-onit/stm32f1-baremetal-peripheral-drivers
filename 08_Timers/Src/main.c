957bb5424b011187fb1f83baa0006b88cf81da24
R-onit <ronitpatil0103@gmail.com>
2025-09-30 07:16:25 +0530

    feat: added tim.c and tim.h ,implementing simple delay using timers

--- 08_Timers/Src/main.c
+++ 08_Timers/Src/main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include "stm32f103xb.h"
-#include "adc.h"
-#include "uart.h"
-#include "systic.h"
+#include "tim.h"
 
+#define SR_UIF				(1U<<0)
 int main(void)
 {
+
 	//to check the systick we are implementing the led[ PA5 ] blink example
 	RCC->APB2ENR |=RCC_APB2ENR_IOPAEN;
 
 	GPIOA->CRL &= ~(0xF<<(5*4));
 	GPIOA->CRL |=  (0x2<<(5*4));
-//0010
-    while (1)
-    {
-    	GPIOA->ODR ^=(1U<<5);
-    	systick_delay_ms(1000);
 
+	tim2_1hz_init();
+	while (1)
+    {
+		while(!(TIM2->SR & SR_UIF )){}
+		TIM2->SR &= ~SR_UIF;
+    	GPIOA->ODR ^=(1U<<5);
     }
 }
